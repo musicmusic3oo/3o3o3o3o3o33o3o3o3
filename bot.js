ac7e9afa79153d2d21adb5609f2dc6b39933a04a
musicmusic3oo <42092091+musicmusic3oo@users.noreply.github.com>
2018-08-08 11:06:51 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ const fetchVideoInfo = require('youtube-info');
 const yt_api_key = "AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4";
 const prefix = '1';
 client.on('ready', function() {
-    client.user.setGame(`KND CLAN`,"http://twitch.tv/sytra_ayman")
+    client.user.setGame(`KINGS NEVER DIE .`,"http://twitch.tv/sytra_ayman")
     console.log(`i am ready ${client.user.username}`);
 });
 /*
